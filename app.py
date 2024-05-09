df6ddf2d0c423215add2c7a88b45cce6c6f1f0f4
phisles <philip.isles@gmail.com>
2024-05-08 20:10:01 -0700

    nltk

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
 import os
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+import nltk
 import warnings
 
 # Suppress specific FutureWarnings from huggingface_hub
 warnings.filterwarnings("ignore", category=FutureWarning, module="huggingface_hub")
 
-print("TRANSCRIPT SENTIMENT ANALYSIS - SCORE OF 0-4 (0 = NEGATIVE, 4 = POSITIVE)")
-
 # Initialize the tokenizer and model
 tokenizer = BertTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = BertForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
-def tokenize_and_classify(content, max_length):
-    start = 0
-    while start < len(content):
-        # Take a substring of the content
-        substring = content[start:start+max_length]
-        tokens = tokenizer.encode(substring, add_special_tokens=True)
-        if len(tokens) > 512:
-            # Reduce the substring length if the token count is exceeded
-            while len(tokens) > 512:
-                substring = substring[:-100]  # Trim substring
-                tokens = tokenizer.encode(substring, add_special_tokens=True)
-        yield substring, tokens
-        start += len(substring)
+# Load NLTK's Punkt tokenizer for sentence splitting
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+
+def tokenize_and_classify(content):
+    # Split the content into sentences using NLTK
+    sentences = sent_tokenize(content)
+    for sentence in sentences:
+        tokens = tokenizer.encode(sentence, add_special_tokens=True)
+        # Check if the tokens exceed the maximum allowed, and skip if they do
+        if len(tokens) <= 512:
+            yield sentence, tokens
 
 def classify_tokens(tokens):
     input_ids = torch.tensor([tokens]).to(model.device)
@@ -43,10 +41,9 @@ for filename in os.listdir(directory_path):
         with open(file_path, 'r') as file:
             content = file.read()
             print(f"----File: {filename}----")
-            for text_snippet, tokens in tokenize_and_classify(content, 1000):  # Character-based chunk size
+            for text_snippet, tokens in tokenize_and_classify(content):
                 label = classify_tokens(tokens)
                 print(f"Sentiment Score: {label}")
-                print(f"Text Snippet: {text_snippet[:60]}...")  # Print the first 50 characters of the snippet and a line break
+                print(f"Text Snippet: {text_snippet}\n")
             print("_" * 50)  # Prints a line of underscores after each file's output
             print("\n")
-#test
